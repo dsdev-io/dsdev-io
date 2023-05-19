fc385462d839bcbde0c791e571b3aed8f17fb194
Dave Shoreman <dshoreman@users.noreply.github.com>
2023-05-19 01:40:54 +0100

    Jazz things up a little, update navbar link

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,47 +6,62 @@
 <div class="text-column">
   <h1>Personal Projects</h1>
 
-    <section>
+  <section>
     <div class="project">
-        <h3>Nextshot</h3>
-        <ul class="tags">
-            <li>Linux</li>
-            <li>Desktop</li>
-            <li>Bash</li>
-        </ul>
+      <h3>Nextshot</h3>
+      <p>A minimal screenshot tool that can upload to NextCloud.</p>
+      <hr />
+      <ul class="tags">
+        <li>Linux</li>
+        <li>Bash</li>
+      </ul>
     </div>
     <div class="project">
-        <h3>Servidor</h3>
-        <ul class="tags">
-            <li>Linux</li>
-            <li>Server</li>
-            <li>PHP</li>
-            <li>Bash</li>
-            <li>Laravel</li>
-            <li>Vue</li>
-        </ul>
+      <h3>Servidor</h3>
+      <p>Linux server admin panel, created purely to learn Vue.</p>
+      <hr />
+      <ul class="tags">
+        <li>Linux</li>
+        <li>Bash</li>
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>Vue</li>
+      </ul>
     </div>
-    </section>
+  </section>
 </div>
 
 <style>
-section { display: flex; width: 48rem; }
-.project {
-    border: 1px solid black;
+  section {
+    display: flex;
+    width: 59rem;
+  }
+  .project {
+    background: var(--color-bg-2);
+    border: 1px solid var(--color-bg-0);
     width: 30rem;
     margin: 0 0.5rem 0;
     padding: 0 1rem 0;
     flex-direction: left;
-}
-.tags {
+  }
+  .tags {
     display: flex;
     list-style: none;
     padding: 0;
-}
-.tags li {
-    background: yellow;
+  }
+  .tags li {
+    background: var(--color-bg-2);
+    border: 1px solid var(--color-bg-0);
     border-radius: 4px;
-    padding: 2px 5px;
+      color: var(--color-theme-2);
+      cursor: default;
+    padding: 4px 5px 2px;
     margin: 0 3px;
-}
+  }
+  hr {
+      border-top: 1px solid var(--color-bg-0);
+      width: 105%;
+      margin-left: -2.5%;
+      padding: 0;
+  }
 </style>
